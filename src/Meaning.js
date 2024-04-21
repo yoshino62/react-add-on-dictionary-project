70f8dcdbff055ac145f1309e58784493238f9986
yoshino62 <150293831+yoshino62@users.noreply.github.com>
2024-04-21 19:30:55 -0400

    Deleted console log

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Meaning(props) {
-  console.log(props.meaning.definitions);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
