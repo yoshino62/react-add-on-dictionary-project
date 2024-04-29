ceb1068e2aa67dd39b4294ff047b2a15d91bf9b9
yoshino62 <150293831+yoshino62@users.noreply.github.com>
2024-04-28 20:09:35 -0400

    Added the links to footer

--- src/App.js
+++ src/App.js
@@ -9,7 +9,33 @@ function App() {
         <main>
           <Dictionary defaultKeyword="sunset" />
         </main>
-        <footer className="App-footer">This is coded by Yoshino</footer>
+        <footer className="App-footer">
+          This is coded by{" "}
+          <a
+            href="https://github.com/yoshino62"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Yoshino Andrews
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/yoshino62/react-add-on-dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and
+          <a
+            href="https://dictionary-project-react-add-on.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
